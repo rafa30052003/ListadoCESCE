a8c2c0cd5006cc36342b6ab9133699b3203e0d62
rafa30052003 <rafalucenavalle9@gmail.com>
2024-04-30 10:21:08 +0200

    cambios en App

--- src/main/java/com/rioma/listacesce/HelloApplication.java
+++ src/main/java/com/rioma/listacesce/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FacturaView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/rioma/listacesce/FacturaView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1181, 813);
         stage.setTitle("Hello!");
         stage.setScene(scene);
